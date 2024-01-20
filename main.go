aad3092c10b30e6e01f0ef69c2ffdc42e380756f
Camden Withrow <withrowcamden@gmail.com>
2024-01-20 15:22:18 -0600

    add websocket append and communicate

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
@@ -34,9 +40,13 @@ func startProxyServer(target string, port string) {
 	url, _ := url.Parse(target)
 	proxy := httputil.NewSingleHostReverseProxy(url)
 
+	proxy.ModifyResponse = modifyResponse
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		proxy.ServeHTTP(w, r)
 	})
+
+	http.HandleFunc("/ws", handleWebSocket)
 
 	fmt.Printf("Starting proxy server of %s at http://localhost:%s\n", target, port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
@@ -56,4 +66,68 @@ func monitorServer(target string) {
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
+}
+
+func modifyResponse(res *http.Response) error {
+	contentTypes := strings.Split(res.Header.Get("Content-Type"), ";")
+	isHTML := false
+	for _, contentType := range contentTypes {
+		if strings.TrimSpace(contentType) == "text/html" {
+			isHTML = true
+		}
+	}
+
+	if isHTML {
+		htmlBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		err = res.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		// Append WebSocket script to the end of the body
+		html := string(htmlBytes)
+		bodyClosingTag := "</body>"
+		script := `<script>(function() { var ws = new WebSocket('ws://localhost:8000/ws'); ws.onmessage = function() { window.location.reload(); }; })();</script>`
+
+		pos := strings.LastIndex(html, bodyClosingTag)
+		newHTML := htmlBytes
+		if pos != -1 {
+			newHTML = []byte(html[:pos] + script + html[pos:])
+		}
+
+		res.Body = io.NopCloser(bytes.NewReader(newHTML))
+		res.ContentLength = int64(len(newHTML))
+		res.Header.Set("Content-Length", strconv.Itoa(len(newHTML)))
+	}
+	return nil
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow connections from any origin
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+
+// handleWebSocket handles incoming WebSocket connections
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	clients[conn] = true
+
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			delete(clients, conn)
+			break
+		}
+	}
+
 }
